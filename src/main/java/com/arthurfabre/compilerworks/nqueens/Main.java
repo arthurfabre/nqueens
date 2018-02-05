4187b1ef1f79b84dacb1dd99a39945d90b82186d
Arthur Fabre <arthur@arthurfabre.com>
2018-02-05 21:16:24 +0100

    Cleanup params and solve interface
    Cleanup command line params, and pass a board around instead of a Params
    object - was a bit overkill.

--- src/main/java/com/arthurfabre/compilerworks/nqueens/Main.java
+++ src/main/java/com/arthurfabre/compilerworks/nqueens/Main.java
@@ -1,17 +1,36 @@
 package com.arthurfabre.compilerworks.nqueens;
 
 import com.beust.jcommander.JCommander;
+import com.beust.jcommander.Parameter;
 
 /**
  *
  */
 public class Main {
 
+    /**
+     * Command line parameters
+     */
+    private static class Params {
+        @Parameter(description = "Size of chess board in rows / columns")
+        private int size = 8;
+
+        @Parameter(names = {"-h", "--help"}, help = true)
+        private boolean help;
+
+        /**
+         * @return Size of a square chess board in rows / columns.
+         */
+        public int getSize() {
+            return size;
+        }
+    }
+
     public static void main(String[] args) {
         Params params = new Params();
         JCommander.newBuilder().addObject(params).build().parse(args);
 
         NQueens nqueens = new NQueens();
-        nqueens.solve(params);
+        nqueens.solve(new Board(params.getSize()));
     }
 }
--- src/main/java/com/arthurfabre/compilerworks/nqueens/NQueens.java
+++ src/main/java/com/arthurfabre/compilerworks/nqueens/NQueens.java
@@ -1,14 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.arthurfabre.compilerworks.nqueens;
 
 /**
  *
- * @author afabre
  */
 public class NQueens {
-    
+
+    public Board solve(Board board) {
+        // TODO
+        return board;
+    }
 }
