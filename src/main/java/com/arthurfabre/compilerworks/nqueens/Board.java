210a7035977a6538859c27913524bab4742e8bff
Arthur Fabre <arthur@arthurfabre.com>
2018-02-06 07:48:01 +0100

    Refactor Board to ensure one queen per row / col
    Instead of verifying we only have one Queen per row or column, change
    our data structure so this is implicit - we store just the row num of
    the queen in each row. This doesn't gurantee row uniqueness, some sort
    of a set would, but there's no nice interface for that in Java.

--- src/main/java/com/arthurfabre/compilerworks/nqueens/Board.java
+++ src/main/java/com/arthurfabre/compilerworks/nqueens/Board.java
@@ -1,33 +1,57 @@
 package com.arthurfabre.compilerworks.nqueens;
 
-import java.util.ArrayList;
-import java.util.Collections;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Set;
 
 /**
  * Chess board, that holds Queens.
  */
 public class Board {
 
-    private final List<List<Boolean>> queens;
+    /**
+     * 
+     */
+    private final List<Integer> positions;
 
-    public Board(int size) {
-        queens = new ArrayList<>(Collections.nCopies(size, new ArrayList<>(Collections.nCopies(size, false))));
+    private Board(List<Integer> positions) {
+        this.positions = positions;
+    }
+
+    public boolean valid() {
+        // Intersection of diagonals and x axis
+        Set<Integer> seenDiagonals = new HashSet<>();
+
+        for (int x = 0; x < positions.size(); x++) {
+            int diagonal = x - positions.get(x);
+
+            if (seenDiagonals.contains(diagonal)) {
+                return false;
+            }
+
+            seenDiagonals.add(diagonal);
+        }
+
+        return true;
     }
 
     /**
-     * Place a queen.
-     * @param x Column
-     * @param y Row
+     * Lazy generated stream of board permutations 
+     * @param size
+     * @return 
      */
-    public void place(int x, int y) {
-        queens.get(x).set(y, true);
-    }
+    public static Iterator<Board> permutations(int size) {
+        return new Iterator<Board>() {
+            @Override
+            public boolean hasNext() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
 
-    /**
-     * @return Size of board in columns / rows.
-     */
-    public int size() {
-        return queens.size();
+            @Override
+            public Board next() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
     }
 }
