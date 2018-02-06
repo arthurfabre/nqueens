451b7c221c53eb2025eec6b04d817e990d0006a5
Arthur Fabre <arthur@arthurfabre.com>
2018-02-06 10:38:35 +0100

    Refactor Main to use Board (intead of old NQueens)
    Refactor Main to use the Board class, instead of the old NQueens one.

--- src/main/java/com/arthurfabre/compilerworks/nqueens/Main.java
+++ src/main/java/com/arthurfabre/compilerworks/nqueens/Main.java
@@ -2,6 +2,7 @@ package com.arthurfabre.compilerworks.nqueens;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import java.util.stream.Stream;
 
 /**
  *
@@ -9,11 +10,14 @@ import com.beust.jcommander.Parameter;
 public class Main {
 
     /**
-     * Command line parameters
+     * Command line parameters.
      */
     private static class Params {
-        @Parameter(description = "Size of chess board in rows / columns")
+        @Parameter(names = {"-s", "--size"}, description = "Size of chess board in rows / columns")
         private int size = 8;
+
+        @Parameter(names = {"-a", "--all"}, description = "Compute all solutions")
+        private boolean all = false;
 
         @Parameter(names = {"-h", "--help"}, help = true)
         private boolean help;
@@ -24,12 +28,39 @@ public class Main {
         public int getSize() {
             return size;
         }
+
+        /**
+         * @return True if we should compute all possible solutions, false otherwise.
+         */
+        public boolean computeAll() {
+            return all;
+        }
+
+        /**
+         * @return True if usage help should be displayed, false otherwise.
+         */
+        public boolean showHelp() {
+            return help;
+        }
     }
 
     public static void main(String[] args) {
         Params params = new Params();
-        JCommander.newBuilder().addObject(params).build().parse(args);
+        JCommander paramParser = JCommander.newBuilder().addObject(params).build();
+        paramParser.parse(args);
 
-        Board.permutations(params.getSize()).filter(Board::valid);
+        if (params.showHelp()) {
+            paramParser.usage();
+            return;
+        }
+
+        Stream<Board> valid = Board.permutations(params.getSize()).filter(Board::valid);
+
+        if (params.computeAll()) {
+            System.out.println(valid.count());
+            return;
+        }
+
+        System.out.println(valid.findFirst().orElseThrow(() -> new RuntimeException("No solutions found")));
     }
 }
